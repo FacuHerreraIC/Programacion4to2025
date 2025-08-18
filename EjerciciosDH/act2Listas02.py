0222a9b633352321dd62577ab7d30abc22bf1a75
Facundo Herrera <facundo.herrera@mi.unc.edu.ar>
2025-08-18 15:57:12 -0300

    Actividad 2 listas 02

---
+++ EjerciciosDH/act2Listas02.py
@@ -0,0 +1,13 @@
+# Lista de puntajes inicial 
+puntajes = [850, 920, 670, 750, 830, 1000]
+minimo = min(puntajes)
+print(minimo)
+maximo = max(puntajes)
+print(maximo)
+promedio = sum(puntajes) / len(puntajes)
+print(promedio)
+puntajes.reverse()
+print(puntajes)
+puntajes.sort()
+print("El TOP SCORE final es: ")
+print(puntajes)
